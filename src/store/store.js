8ac6f81e67880ba1d79e706396bac1a038cb6d1f
keipy27 <kevinandris27@gmail.com>
2023-09-13 12:23:20 +1200

    Provide the store for app.js

--- src/store/store.js
+++ src/store/store.js
@@ -3,17 +3,22 @@ import { storeCreator } from "redux";
 // ! 2) Reducer
 const countReducer = (state = 0, action) => {
     if (action.type === "SUBTRACT") {
-        return state - 1
+        return state - 1;
     }
     if (action.type === "ADD") {
-        return state + 1
+        return state + 1;
     }
     if (action.type === "RESET") {
-        return state = 0
+        return (state = 0);
     }
+
+    return state;
 };
 
 // ! 1) create a store
-const store = storeCreator(countReducer);
+const store = storeCreator(
+    countReducer,
+    window._REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION()
+);
 
 export default store;
